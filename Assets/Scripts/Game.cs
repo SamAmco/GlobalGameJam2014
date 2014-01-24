403ae0469555486c7fde05c3f20695310e7aa5c9
rgmoreno <garcia.moreno.ruben@gmail.com>
2014-01-24 22:41:44 +0000

    Basic Logic

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -3,6 +3,52 @@ using System.Collections;
 
 public class Game : MonoBehaviour
 {
+    public GameObject player01Prefab;
+    public GameObject player02Prefab;
 
+    private Player player01;
+    private Player player02;
 
+    public World world01;
+    public World world02;
+
+    void Start()
+    {
+        player01 = (GameObject.Instantiate(player01Prefab) as GameObject).GetComponent<Player>();
+        player02 = (GameObject.Instantiate(player02Prefab) as GameObject).GetComponent<Player>();
+
+        player01.transform.parent = world01.transform;
+        player02.transform.parent = world02.transform;
+
+        player01.transform.localPosition = world01.spawnPoint.localPosition;
+        player02.transform.localPosition = world02.spawnPoint.localPosition;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            swapPlayers();
+        }
+    }
+
+    private void swapPlayers()
+    {
+        Vector3 player01LocalPosition = player01.transform.localPosition;
+        Vector3 player02LocalPosition = player02.transform.localPosition;
+
+        Transform player01Parent = player01.transform.parent;
+        Transform player02Parent = player02.transform.parent;
+
+        player01.transform.parent = player02Parent;
+        player02.transform.parent = player01Parent;
+
+        player01.transform.localPosition = player02LocalPosition;
+        player02.transform.localPosition = player01LocalPosition;
+    }
+
+    public void playerReachedEnd(Player player)
+    {
+
+    }
 }
