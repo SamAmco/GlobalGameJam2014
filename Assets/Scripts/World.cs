d2eb2452450ac3573630f29c1966b18e55b1a71c
rgmoreno <garcia.moreno.ruben@gmail.com>
2014-01-24 22:45:54 +0000

    fixed git problem with directories

---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class World : MonoBehaviour
+{
+    public Transform spawnPoint;
+    public EndOfLevel endOfLevel;
+
+    public void init(Game game)
+    {
+        endOfLevel.init(game);
+    }
+}
